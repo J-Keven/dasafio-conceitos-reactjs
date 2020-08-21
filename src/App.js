5ed5486a4ce4e6fe2ba689f41800aae0f7a5e2da
J-keven <jhonasnunes425@gmail.com>
2020-08-21 10:49:48 -0300

    [feat]: challenge finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  const handleLoadRepositories = async () => {
+    const response = await api.get('repositories')
+
+    setRepositories(response.data)
+  }
+
   async function handleAddRepository() {
-    // TODO
+    // const newRepositry = {
+    //   title: "desafio-conceitos-reactjs",
+    //   url: "https://github.com/j-keven/desafio-conceitos-node",
+    //   techs: ["javascript"]
+    // }
+
+    const response = await api.post('repositories', {
+      title: "desafio-conceitos-reactjs",
+      url: "https://github.com/j-keven/desafio-conceitos-node",
+      techs: ["javascript"]
+    })
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories([...repositories.filter(item => item.id !== id)])
   }
+
+  useEffect(() => {
+    handleLoadRepositories();
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(item => {
+            return (
+              <li key={item.id}>
+                {
+                  item.title
+                }
+                <button onClick={() => handleRemoveRepository(item.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
